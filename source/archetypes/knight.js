84e58a93e22451a4de8dc328ca08f4f28d664412
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-28 12:24:50 -0700

    port moveDAO functions

--- source/archetypes/knight.js
+++ source/archetypes/knight.js
@@ -1,5 +1,4 @@
 const { ArchetypeTemplate } = require("../classes");
-// const { getTargetList } = require("../moveDAO.js");
 
 module.exports = new ArchetypeTemplate("Knight",
 	"Able to predict which allies enemies are targeting with which moves, the Knight excels at efficiently mitigating damage.",
@@ -10,7 +9,7 @@ module.exports = new ArchetypeTemplate("Knight",
 			if (userReference.team === "enemy") {
 				const enemy = adventure.getCombatant(userReference);
 				if (enemy.hp > 0) {
-					const targetNames = getTargetList(targets, adventure);
+					const targetNames = targets.map(reference => adventure.getCombatant(reference).getName(adventure.room.enemyIdMap));
 					if (name !== "@{clone}") {
 						embed.addFields({ name: enemy.getName(adventure.room.enemyIdMap), value: `Round ${adventure.room.round + 1}: ${name} ${priority != 0 ? "(Priority: " + priority + ") " : ""}(Targets: ${targetNames.length ? targetNames.join(", ") : "???"})\nRound ${adventure.room.round + 2}: ${enemy.nextAction}` });
 					} else {
--- source/orcustrators/adventureOrcustrator.js
+++ source/orcustrators/adventureOrcustrator.js
@@ -21,7 +21,6 @@ const { spawnEnemy } = require("../util/roomUtil");
 const { parseExpression } = require("../util/textUtil");
 
 // const { getGuild, setGuild } = require("./guildDAO.js");
-// const { resolveMove } = require("./moveDAO.js");
 // const { renderRoom, updateRoomHeader } = require("./roomDAO.js");
 
 /** @type {Map<string, Adventure>} */
@@ -359,6 +358,106 @@ function newRound(adventure, thread, lastRoundText) {
 	});
 }
 
+/** Updates game state with the move's effect AND returns the game's description of what happened
+ * @param {Move} move
+ * @param {Adventure} adventure
+ * @returns {Promise<string>} result text
+ */
+async function resolveMove(move, adventure) {
+	const user = adventure.getCombatant(move.userReference);
+	if (!user || user.hp < 1) {
+		return "";
+	}
+
+	let moveText = `**${user.getName(adventure.room.enemyIdMap)}** `;
+	if (move.name !== "Stun" && user.getModifierStacks("Stun") < 1) {
+		if (move.isCrit) {
+			moveText = `💥${moveText}`;
+		}
+
+		let effect;
+		let breakText = "";
+		switch (move.type) {
+			case "action":
+				if (move.userReference.team !== "delver") {
+					const action = getEnemy(user.archetype).actions[move.name];
+					let parsedElement = action.element;
+					if (parsedElement === "@{adventure}") {
+						parsedElement = adventure.element;
+					} else if (parsedElement === "@{adventureOpposite}") {
+						parsedElement = getOpposite(adventure.element);
+					}
+					effect = action.effect;
+					moveText = `${getEmoji(parsedElement)} ${moveText}`;
+				}
+				break;
+			case "gear":
+				effect = getGearProperty(move.name, "effect");
+				if (move.name !== "Punch" && move.userReference.team !== "enemy") {
+					let decrementDurability = true;
+					const gearCategory = getGearProperty(move.name, "category");
+					if (gearCategory === "Spell") {
+						const crystalShardCount = adventure.getArtifactCount("Crystal Shard");
+						if (crystalShardCount > 0) {
+							const durabilitySaveChance = 1 - 0.85 ** crystalShardCount;
+							const max = 144;
+							adventure.updateArtifactStat("Crystal Shard", "Expected Durability Saved", durabilitySaveChance.toFixed(2));
+							if (adventure.generateRandomNumber(max, "battle") < max * durabilitySaveChance) {
+								decrementDurability = false;
+								adventure.updateArtifactStat("Crystal Shard", "Actual Durability Saved", 1);
+							}
+						}
+					}
+					if (decrementDurability) {
+						const gear = user.gear.find(gear => gear.name === move.name);
+						gear.durability--;
+						if (gear.durability < 1) {
+							breakText = ` The ${move.name} broke!`;
+						}
+					}
+				}
+				moveText = `${getEmoji(getGearProperty(move.name, "element"))} ${moveText}`;
+				break;
+			case "item":
+				const { effect: itemEffect, element } = getItem(move.name);
+				effect = itemEffect;
+				if (move.userReference.team !== "enemy") {
+					adventure.items[move.name]--;
+					if (adventure.items[move.name] < 1) {
+						delete adventure.items[move.name];
+					}
+				}
+				moveText = `${getEmoji(element)} ${moveText}`;
+				break;
+		}
+
+		const targets = move.targets.map(targetReference => adventure.getCombatant(targetReference)).filter(reference => !!reference);
+		const resultText = await effect(targets, adventure.getCombatant(move.userReference), move.isCrit, adventure);
+
+		moveText += `used ${move.name}. ${resultText}${breakText}`;
+	} else {
+		removeModifier(user, { name: "Stun", stacks: "all" });
+		moveText = `💫 ${moveText} is Stunned!`;
+	}
+
+	// Poison/Regen
+	const poisonStacks = user.getModifierStacks("Poison");
+	let poisonDamage = poisonStacks * 10;
+	if (user.team === "enemy") {
+		const funnelDamage = adventure.getArtifactCount("Spiral Funnel") * 5 * poisonStacks;
+		poisonDamage += funnelDamage;
+		adventure.updateArtifactStat("Spiral Funnel", "Additional Damage", funnelDamage);
+	}
+
+	const regenStacks = user.getModifierStacks("Regen");
+	if (poisonDamage) {
+		moveText += ` ${await dealDamage([user], null, poisonDamage, true, "Poison", adventure)}`;
+	} else if (regenStacks) {
+		moveText += ` ${gainHealth(user, regenStacks * 10, adventure)}`;
+	}
+	return `${moveText}\n`;
+}
+
 /** Generate reactive moves, randomize speed ties, then resolve moves
  * @param {Adventure} adventure
  * @param {ThreadChannel} thread
@@ -400,6 +499,7 @@ async function endRound(adventure, thread) {
 	let lastRoundText = "";
 	for (const move of adventure.room.moves) {
 		lastRoundText += await resolveMove(move, adventure);
+
 		// Check for end of combat
 		if (adventure.lives <= 0) {
 			adventure.room.round++;
@@ -429,6 +529,11 @@ async function endRound(adventure, thread) {
 
 			return thread.send(renderRoom(adventure, thread, lastRoundText));
 		}
+
+		// remove Slow and Quicken
+		const moveUser = adventure.getCombatant(move.userReference);
+		removeModifier(moveUser, { name: "Slow", stacks: 1, force: true });
+		removeModifier(moveUser, { name: "Quicken", stacks: 1, force: true });
 	}
 	adventure.room.moves = [];
 	newRound(adventure, thread, lastRoundText);
