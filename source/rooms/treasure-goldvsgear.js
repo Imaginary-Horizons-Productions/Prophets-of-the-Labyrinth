38f423df1b2d86da1775be36273d0f906d68d9e0
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-27 22:08:26 -0700

    fix treasure room typos

--- source/rooms/treasure-goldvsgear.js
+++ source/rooms/treasure-goldvsgear.js
@@ -2,7 +2,7 @@ const { RoomTemplate, ResourceTemplate } = require("../classes");
 
 module.exports = new RoomTemplate("Treasure! Gold or Gear?",
 	"@{adventure}",
-	"Two treasure boxes sit on opposite ends of a seesaw suspended above pits of molten rock. They are labled 'Gold' and 'Equipment' respectively, and it looks as if taking one will surely cause the other to plummet into the pit below.",
+	"Two treasure boxes sit on opposite ends of a seesaw suspended above pits of molten rock. They are labled 'Gold' and 'Gear' respectively, and it looks as if taking one will surely cause the other to plummet into the pit below.",
 	[
 		new ResourceTemplate("1", "internal", "roomAction"),
 		new ResourceTemplate("250*n", "always", "gold").setCostMultiplier(0),
--- source/rooms/treasure-goldvsitems.js
+++ source/rooms/treasure-goldvsitems.js
@@ -1,8 +1,8 @@
 const { RoomTemplate, ResourceTemplate } = require("../classes");
 
-module.exports = new RoomTemplate("Treasure! Gold or Consumables?",
+module.exports = new RoomTemplate("Treasure! Gold or Items?",
 	"@{adventure}",
-	"Two treasure boxes sit on opposite ends of a seesaw suspended above pits of molten rock. They are labled 'Gold' and 'Consumable Bundle' respectively, and it looks as if taking one will surely cause the other to plummet into the pit below.",
+	"Two treasure boxes sit on opposite ends of a seesaw suspended above pits of molten rock. They are labled 'Gold' and 'Item Bundle' respectively, and it looks as if taking one will surely cause the other to plummet into the pit below.",
 	[
 		new ResourceTemplate("1", "internal", "roomAction"),
 		new ResourceTemplate("250*n", "always", "gold").setCostMultiplier(0),
