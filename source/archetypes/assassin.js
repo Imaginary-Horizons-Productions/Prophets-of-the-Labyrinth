4800ca20abe41ad31057aa5ea5c1f234a1629b75
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-10-04 17:50:47 -0700

    update assassin predict for more elements

--- source/archetypes/assassin.js
+++ source/archetypes/assassin.js
@@ -1,5 +1,5 @@
 const { ArchetypeTemplate } = require("../classes");
-const { getEmoji, getWeaknesses } = require("../util/elementUtil");
+const { getEmoji, getWeaknesses, getResistances } = require("../util/elementUtil");
 
 module.exports = new ArchetypeTemplate("Assassin",
 	"Able to predict which combatants will critically hit and assess combatant elemental affinities, the Assassin excels at dealing great amounts of damage.",
@@ -7,7 +7,9 @@ module.exports = new ArchetypeTemplate("Assassin",
 	["Daggers", "Cloak"],
 	(embed, adventure) => {
 		adventure.room.enemies.filter(combatant => combatant.hp > 0).concat(adventure.delvers).forEach(combatant => {
-			embed.addFields({ name: `${combatant.getName(adventure.room.enemyIdMap)} ${getEmoji(combatant.element)}`, value: `Critical Hit: ${combatant.crit ? "💥" : "🚫"}\nWeakness: ${getEmoji(getWeakness(combatant.element))}\nResistance: ${getEmoji(combatant.element)}` });
+			const weaknesses = getWeaknesses(combatant.element);
+			const resistances = getResistances(combatant.element);
+			embed.addFields({ name: `${combatant.getName(adventure.room.enemyIdMap)} ${getEmoji(combatant.element)}`, value: `Critical Hit: ${combatant.crit ? "💥" : "🚫"}\nWeakness: ${weaknesses.map(weakness => getEmoji(weakness)).join(" ")}\nResistance: ${resistances.map(resistance => getEmoji(resistance)).join(" ")}` });
 		});
 		return [false, embed]
 	},
