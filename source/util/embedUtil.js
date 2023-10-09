ddabbb22b672d926b9dc2ff997d6d8ae91f9b89f
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-10-09 11:01:33 -0700

    update generateVersionEmbed

--- source/util/embedUtil.js
+++ source/util/embedUtil.js
@@ -244,11 +244,8 @@ function updateRoomHeader(adventure, message) {
 	message.edit({ embeds: message.embeds.map(embed => new EmbedBuilder(embed).setAuthor({ name: roomHeaderString(adventure), iconURL: message.client.user.displayAvatarURL() })) })
 }
 
-/** The version embed lists the following: changes in the most recent update, known issues in the most recent update, and links to support the project
- * @param {string} avatarURL
- * @returns {EmbedBuilder}
- */
-async function generateVersionEmbed(avatarURL) {
+/** The version embed lists the following: changes in the most recent update, known issues in the most recent update, and links to support the project */
+async function generateVersionEmbed() {
 	const data = await fs.promises.readFile('./ChangeLog.md', { encoding: 'utf8' });
 	const dividerRegEx = /## .+ Version/g;
 	const changesStartRegEx = /\.\d+:/g;
@@ -262,12 +259,10 @@ async function generateVersionEmbed(avatarURL) {
 	}
 	let knownIssuesEnd = dividerRegEx.exec(data).index;
 
-	const embed = new EmbedBuilder().setColor(Colors.Blurple)
-		.setAuthor({ name: "Click here to check out the Imaginary Horizons GitHub", iconURL: avatarURL, url: "https://github.com/Imaginary-Horizons-Productions" })
+	const embed = embedTemplate()
 		.setTitle(data.slice(titleStart + 3, changesStartRegEx.lastIndex))
 		.setURL('https://discord.gg/JxqE9EpKt9')
 		.setThumbnail('https://cdn.discordapp.com/attachments/545684759276421120/734099622846398565/newspaper.png')
-		.setFooter({ text: "Imaginary Horizons Productions", iconURL: "https://cdn.discordapp.com/icons/353575133157392385/c78041f52e8d6af98fb16b8eb55b849a.png" })
 		.setTimestamp();
 
 	if (knownIssuesStart && knownIssuesStart < knownIssuesEnd) {
@@ -278,7 +273,7 @@ async function generateVersionEmbed(avatarURL) {
 		// Known Issues section not found
 		embed.setDescription(data.slice(changesStartRegEx.lastIndex, knownIssuesEnd));
 	}
-	return embed.addFields({ name: "Become a Sponsor", value: "Chip in for server costs or get premium features by sponsoring [{bot} on GitHub]( url goes here )" });
+	return embed.addFields({ name: "Become a Sponsor", value: "Chip in for server costs or get premium features by sponsoring [PotL on GitHub](https://github.com/Imaginary-Horizons-Productions/Prophets-of-the-Labyrinth)" });
 }
 
 /**
