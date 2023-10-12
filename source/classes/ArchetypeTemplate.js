6f51da595fc0cffd35bab75e90bee68a01f1c027
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-10-12 15:10:15 -0700

    update typing in ArchetypeTemplate

--- source/classes/ArchetypeTemplate.js
+++ source/classes/ArchetypeTemplate.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder } = require("discord.js");
 const { Adventure } = require("./Adventure");
 const { BuildError } = require("./BuildError");
+const { Combatant } = require("./Combatant");
 
 class ArchetypeTemplate {
 	/**
@@ -9,7 +10,7 @@ class ArchetypeTemplate {
 	 * @param {"Darkness" | "Earth" | "Fire" | "Light" | "Water" | "Wind" | "Untyped"} elementLabel
 	 * @param {string[]} startingGearNames
 	 * @param {(embed: EmbedBuilder, adventure: Adventure ) => EmbedBuilder} predictFunction
-	 * @param {(combatant) => string} miniPredictFunction
+	 * @param {(combatant:Combatant) => string} miniPredictFunction
 	 */
 	constructor(nameInput, descriptionInput, elementLabel, startingGearNames, predictFunction, miniPredictFunction) {
 		if (!nameInput) throw new BuildError("Falsy nameInput");
