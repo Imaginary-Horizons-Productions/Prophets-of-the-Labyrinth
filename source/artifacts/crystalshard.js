c8c005080026f6f0b15c7c84c216790d50280714
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-01-04 13:56:38 -0800

    apply Vexing bonus to Prismatic Blast targets

--- source/artifacts/crystalshard.js
+++ source/artifacts/crystalshard.js
@@ -2,6 +2,6 @@ const { ArtifactTemplate } = require("../classes");
 
 module.exports = new ArtifactTemplate("Crystal Shard",
 	"Increase the blast range of Spells by @{copies}. Single target Spells become Blast @{copies}.",
-	"Increase range boost 1 per shard",
+	"Increase range boost by 1 per shard",
 	"Untyped"
 );
--- source/gear/prismaticblast-vexing.js
+++ source/gear/prismaticblast-vexing.js
@@ -9,17 +9,32 @@ module.exports = new GearTemplate("Vexing Prismatic Blast",
 	"Light",
 	350,
 	(targets, user, isCrit, adventure) => {
-		const { element, damage, critMultiplier } = module.exports;
-		let pendingDamage = user.getPower() + damage;
-		if (user.element === element) {
-			targets.forEach(target => {
+		const { element, damage, critMultiplier, bonus } = module.exports;
+		let debuffedDamage = user.getPower() + damage + bonus;
+		let nonDebuffedDamage = user.getPower() + damage;
+		const debuffedTargets = [];
+		const nondebuffedTargets = [];
+		targets.forEach(target => {
+			if (user.element === element) {
 				target.addStagger("elementMatchFoe");
-			})
-		}
+			}
+			if (Object.keys(target.modifiers).some(modifier => isDebuff(modifier))) {
+				debuffedTargets.push(target);
+			} else {
+				nondebuffedTargets.push(target);
+			}
+		})
 		if (isCrit) {
-			pendingDamage *= critMultiplier;
+			debuffedDamage *= critMultiplier;
+			nonDebuffedDamage *= critMultiplier;
 		}
-		return dealDamage(targets, user, pendingDamage, false, element, adventure);
+		if (debuffedTargets.length > 0 && nondebuffedTargets.length > 0) {
+			return `${dealDamage(debuffedTargets, user, debuffedDamage, false, element, adventure)} ${dealDamage(nondebuffedTargets, user, nonDebuffedDamage, false, element, adventure)}`;
+		} else if (debuffedTargets.length > 0) {
+			return dealDamage(debuffedTargets, user, debuffedDamage, false, element, adventure);
+		} else {
+			return dealDamage(nondebuffedTargets, user, nonDebuffedDamage, false, element, adventure);
+		}
 	}
 ).setTargetingTags({ target: `blast${SAFE_DELIMITER}1`, team: "foe", needsLivingTargets: true })
 	.setDurability(15)
