1ff17ce2d300597769527fe3de3824f73d51e4ca
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-01-18 12:09:08 -0800

    fix typo in fire arrow frog fight description

--- source/rooms/battle-firearrowfrogs.js
+++ source/rooms/battle-firearrowfrogs.js
@@ -3,7 +3,7 @@ const { generateCombatRoomBuilder } = require("../util/messageComponentUtil");
 
 module.exports = new RoomTemplate("Frog Fight",
 	"Fire",
-	"A blaze of orange and red in the muck the outs itself as a warning sign to a blast of heated mud and venom.",
+	"A blaze of orange and red in the muck outs itself as a warning sign to a blast of heated mud and venom.",
 	[
 		new ResourceTemplate("1", "internal", "levelsGained"),
 		new ResourceTemplate("25*n", "loot", "gold")
