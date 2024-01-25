5946c12d38b3e7f4d279f571ad66c2b827fd1203
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-01-25 13:06:27 -0800

    #122: Placebo rework (#127)

--- source/buttons/buylife.js
+++ source/buttons/buylife.js
@@ -1,4 +1,5 @@
 const { ButtonWrapper } = require('../classes');
+const { SAFE_DELIMITER } = require('../constants');
 const { getAdventure, setAdventure } = require('../orcustrators/adventureOrcustrator');
 const { updateRoomHeader } = require('../util/embedUtil');
 const { editButtons } = require('../util/messageComponentUtil');
@@ -6,7 +7,7 @@ const { editButtons } = require('../util/messageComponentUtil');
 const mainId = "buylife";
 module.exports = new ButtonWrapper(mainId, 3000,
 	/** -50 score, +1 life */
-	(interaction, args) => {
+	(interaction, [boughtWith]) => {
 		const adventure = getAdventure(interaction.channelId);
 		if (!adventure?.delvers.some(delver => delver.id === interaction.user.id)) {
 			interaction.reply({ content: "This adventure isn't active or you aren't participating in it.", ephemeral: true });
@@ -14,11 +15,27 @@ module.exports = new ButtonWrapper(mainId, 3000,
 		}
 
 		adventure.lives++;
-		adventure.score -= 50;
-		updateRoomHeader(adventure, interaction.message);
-		const updatedUI = editButtons(interaction.message.components, { [interaction.customId]: { preventUse: true, label: "-50 score, +1 life", emoji: "✔️" } });
-		interaction.update({ components: updatedUI }).then(() => {
-			setAdventure(adventure);
+		if (boughtWith === "score") {
+			adventure.score -= 50;
+		} else {
+			adventure.decrementItem("Placebo", 1);
+		}
+		updateRoomHeader(adventure, interaction.message).then(message => {
+			let updatedUI;
+			if (boughtWith === "score") {
+				updatedUI = editButtons(message.components, {
+					[`${mainId}${SAFE_DELIMITER}score`]: { preventUse: true, label: "+1 life", emoji: "✔️" },
+					[`${mainId}${SAFE_DELIMITER}placebo`]: { preventUse: true, label: "-50 score", emoji: "✖️" }
+				});
+			} else {
+				updatedUI = editButtons(message.components, {
+					[`${mainId}${SAFE_DELIMITER}score`]: { preventUse: true, label: "-1 Placebo", emoji: "✖️" },
+					[`${mainId}${SAFE_DELIMITER}placebo`]: { preventUse: true, label: "+1 life", emoji: "✔️" }
+				});
+			}
+			interaction.update({ components: updatedUI }).then(() => {
+				setAdventure(adventure);
+			});
 		});
 	}
 );
