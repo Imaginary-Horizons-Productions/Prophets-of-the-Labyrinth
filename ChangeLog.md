9e85139c3dec7e4bb6ef54d115c9b66d7b908f0f
doubledh <henryh2014@gmail.com>
2024-03-07 11:42:52 -0600

    Add joke predicts for fighter (#194)
    Co-authored-by: Henry Hu <henryhu@hufamily.com>

--- ChangeLog.md
+++ ChangeLog.md
@@ -31,7 +31,7 @@ The Chemist was a powerful archetype, but the gameplay didn't allign with the th
 - New Challenge: Training Weights
 - Reworked the Treasure Elemental ||Instead of a timed life, delvers can spend their turn adding Curse of Midas to the Elemental, which they now must defeat||
 - Combined event rooms "Elemental Research" and "HP Redistribution" into "Imp Contract Faire"
-- New Archetype: Fighter - No predict or extra effects on gear, double stat growths
+- New Archetype: Fighter - Joke predict, simple starting gear, double stat growths
 ## Prophets of the Labyrinth v0.13.2:
 - Fixed a crash caused by new player profiles not rolling starting archetypes
 ## Prophets of the Labyrinth v0.13.1:
--- source/archetypes/fighter.js
+++ source/archetypes/fighter.js
@@ -12,8 +12,21 @@ module.exports = new ArchetypeTemplate("Fighter",
 	},
 	["Strong Attack", "Second Wind"],
 	(embed, adventure) => {
+		let descriptions = [];
+		descriptions.push(
+			`I'm a fighter.`,
+			`I don't have any fancy gear`,
+			`I have double stat growths!`,
+			`¯\\\\(ツ)/¯`,
+			`This delve into ${adventure.name} is exciting!`,
+			`I *think* it's <t:${Math.floor(Date.now() / 1000)}:t>`,
+			`It's high noon (<t:${Math.floor(new Date().setHours(12, 0, 0) / 1000)}:R>)`
+		);
+		if (adventure.delvers.length > 1) {
+			descriptions.push(`I'm so happy to have my ${adventure.delvers.length - 1} good friends with me.`);
+		}
 		return embed.setTitle(`Fighter Predictions for Round ${adventure.room.round + 1}`)
-			.setDescription("¯\\_(ツ)_/¯");
+			.setDescription(descriptions[Date.now() % descriptions.length]);
 	},
 	(combatant) => ""
 );
