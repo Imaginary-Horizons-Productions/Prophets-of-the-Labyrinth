42b3d11d0ea804f5287d0b707109342084fdde58
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-05-13 14:19:17 -0700

    make autocomplete filtering case-insensitive

--- source/bot.js
+++ source/bot.js
@@ -121,7 +121,7 @@ client.on(Events.InteractionCreate, interaction => {
 		if (unfilteredChoices.length < 1) {
 			console.error(`Attempted autocomplete on misconfigured command ${interaction.commandName} ${focusedOption.name}`);
 		}
-		const choices = unfilteredChoices.filter(choice => choice.value.includes(focusedOption.value))
+		const choices = unfilteredChoices.filter(choice => choice.value.toLowerCase().includes(focusedOption.value.toLowerCase()))
 			.slice(0, 25);
 		interaction.respond(choices);
 	} else if (interaction.isCommand()) {
