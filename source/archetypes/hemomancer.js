9871e9be2e7a7a44144dcfa6011c5d2ba33fee3e
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-07-03 12:54:45 -0700

    change predict to HP & Speed

--- source/archetypes/hemomancer.js
+++ source/archetypes/hemomancer.js
@@ -3,7 +3,7 @@ const { getNames } = require("../util/combatantUtil");
 const { generateTextBar } = require("../util/textUtil");
 
 module.exports = new ArchetypeTemplate("Hemomancer",
-	"They'll be able to predict the order combatants will act in and their how much Stagger to Stun them. They'll also be able to redirect enemy attacks with Blood Aegis.",
+	"They'll be able to predict the order combatants will act in and assess HP levels. They'll also be able to redirect enemy attacks with Blood Aegis.",
 	"Darkness",
 	{
 		maxHPGrowth: 25,
@@ -21,16 +21,12 @@ module.exports = new ArchetypeTemplate("Hemomancer",
 			});
 		const combatantNames = getNames(activeCombatants, adventure);
 		activeCombatants.forEach((combatant, index) => {
-			embed.addFields({ name: combatantNames[index], value: `${combatant.isStunned ? "💫 Stunned" : `Stagger: ${generateTextBar(combatant.stagger, combatant.getPoise(), combatant.getPoise())}`}\nSpeed: ${combatant.getSpeed(true)}` });
+			embed.addFields({ name: combatantNames[index], value: `${generateTextBar(combatant.hp, combatant.getMaxHP(), 16)} ${combatant.hp}/${combatant.getMaxHP()} HP${combatant.protection ? `, ${combatant.protection} Protection` : ""}\nSpeed: ${combatant.getSpeed(true)}` });
 		});
 		embed.setDescription("Combatants may act out of order if they have priority or they are tied in speed.");
 		return embed.setTitle(`Hemomancer Predictions for Round ${adventure.room.round + 1}`);
 	},
 	(combatant) => {
-		if (combatant.isStunned) {
-			return "💫 Stunned";
-		} else {
-			return `Stagger: ${generateTextBar(combatant.stagger, combatant.getPoise(), combatant.getPoise())}`;
-		}
+		return `Speed: ${combatant.getSpeed(true)}`;
 	}
 );
