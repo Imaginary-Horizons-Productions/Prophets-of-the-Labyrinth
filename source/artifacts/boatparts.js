64c34dad233f2bb283a98e1a71cdbec3721d0af0
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-07-04 07:39:25 -0700

    #221: Split off first set of element-specific artifacts (#226)

--- source/artifacts/boatparts.js
+++ source/artifacts/boatparts.js
@@ -3,5 +3,5 @@ const { ArtifactTemplate } = require("../classes");
 module.exports = new ArtifactTemplate("Boat Parts",
 	"For the first @{copies+1} rounds of combat, delvers gain @{copies*25+25} protection.",
 	"Increase the number of rounds by 1 and protection gained by 25 for each set of parts",
-	"Untyped"
+	"Earth"
 ).setFlavorText({ name: "*Additional Notes*", value: "Odysseus would be proud." });
