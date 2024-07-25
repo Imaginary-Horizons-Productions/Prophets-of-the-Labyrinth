d5389720848d7858257adf5bf7c15c726c9f73ce
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-07-25 12:41:23 -0700

    add emoji for Lucky and Unlucky

--- source/modifiers/lucky.js
+++ source/modifiers/lucky.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Lucky",
 	"For the next @{stackCount} round(s), your moves roll for Critical Hit @{stackCount+1} times and are Critical Hits if any roll succeeds.",
-	null,
+	"<:Lucky:1266117709297619045>",
 	true,
 	false,
 	1
--- source/modifiers/unlucky.js
+++ source/modifiers/unlucky.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Unlucky",
 	"For the next @{stackCount} round(s), your moves roll for Critical Hit @{stackCount+1} times and are Critical Hits only if half or more rolls succeed.",
-	null,
+	"<:Unlucky:1266117872984260610>",
 	false,
 	true,
 	1
