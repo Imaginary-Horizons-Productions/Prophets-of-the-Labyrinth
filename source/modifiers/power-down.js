f1ab93f03b169adb4b1db4025e468d72ca199a57
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-07-29 16:10:01 -0700

    #249: add emoji for Power Up and Power Down (#261)

--- source/modifiers/power-down.js
+++ source/modifiers/power-down.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Power Down",
 	"Decreases damage dealt by moves by @{stackCount}.",
-	null,
+	"<:PowerDown:1267617436396687493>",
 	false,
 	true,
 	0
