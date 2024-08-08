ea64bb5ce8cb2802d125533118ac2033488d50ce
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-08-08 10:45:18 -0700

    #239: update /about

--- source/commands/about.js
+++ source/commands/about.js
@@ -9,16 +9,16 @@ module.exports = new CommandWrapper(mainId, "Get info and contributors to PotL",
 			embeds: [
 				embedTemplate().setTitle(`Prophets of the Labyrinth ${GAME_VERSION}`)
 					.setThumbnail(interaction.client.user.displayAvatarURL())
-					.setDescription(`A roguelike dungeon crawl in Discord to play with other server members.`)
+					.setDescription("A multiplayer roguelite dungeon crawl to play with Discord server members.")
 					.addFields([
-						{ name: `Design & Engineering`, value: `Nathaniel Tseng ( <@106122478715150336> | [GitHub](https://github.com/ntseng) )` },
-						{ name: `Dev & Review`, value: `Henry Hu ( <@113108081990176768> | [Twitter](https://twitter.com/hdoubledh) )` },
-						{ name: `Boba Dev`, value: `Vivian Thach ( <@334803621827051534> | [Instagram](https://www.instagram.com/bobaguardian/) )` },
+						{ name: "Design & Engineering", value: "Nathaniel Tseng ( <@106122478715150336> | [GitHub](https://github.com/ntseng) )" },
+						{ name: "Dev & Review", value: "Henry Hu ( <@113108081990176768> | [Twitter](https://twitter.com/hdoubledh) )" },
 						{ name: "Random Number Generator", value: "Alex Frank" },
 						{ name: "Room Loader", value: "Michel Momeyer" },
 						{ name: "Predict Balance", value: "Lucas Ensign" },
+						{ name: "Inactive Developers", value: "Vivian Thach" },
 						{ name: "Playtesting", value: "Ralph Beishline, Eric Hu, TheChreative, Jon Puddicombe" },
-						{ name: `Embed Thumbnails`, value: `[game-icons.net](https://game-icons.net/)` }
+						{ name: "Embed Thumbnails & Icons", value: "[game-icons.net](https://game-icons.net/)" }
 					])
 			],
 			ephemeral: true
