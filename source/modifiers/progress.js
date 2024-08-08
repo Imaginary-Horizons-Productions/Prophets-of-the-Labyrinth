eda5d86578d03019915883fbc0bf5704d737ee64
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-08-08 11:30:14 -0700

    #251: add emoji for Progress

--- source/modifiers/progress.js
+++ source/modifiers/progress.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Progress",
 	"The Elkemist completes its potion when progress reaches 100. Stun the Elkemist to inhibit some progress.",
-	null,
+	"<:Progress:1271173306598428672>",
 	false,
 	false,
 	0
