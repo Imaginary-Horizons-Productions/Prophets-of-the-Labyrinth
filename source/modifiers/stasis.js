7f2a0feee29f4ce7f2acc0d008e2e509dd2c3142
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-08-08 11:40:03 -0700

    #247: add emoji for Oblivious and Stasis (#275)

--- source/modifiers/stasis.js
+++ source/modifiers/stasis.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Stasis",
 	"Ignore the next @{stackCount} attempt(s) to remove buffs or debuffs (any number of stacks).",
-	null,
+	"<:Stasis:1271175642771361863>",
 	false,
 	false,
 	0
