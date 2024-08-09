16531c613c4578140c936ca920d4947aeca14ce4
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-08-09 10:17:06 -0700

    #244: add emoji for Curse of Midas

--- source/modifiers/curse-of-midas.js
+++ source/modifiers/curse-of-midas.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Curse of Midas",
 	"Add @{stackCount} gold to loot for each 10 damage the bearer takes from moves.",
-	null,
+	"<:CurseOfMidas:1271517485455380510>",
 	false,
 	true,
 	0
