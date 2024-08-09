2a2d72a568ec4291704803f50dbb9ace2110b296
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-08-09 10:21:32 -0700

    #252: add emoji for Iron Fist Stance and Floating Mist Stance (#284)

--- source/modifiers/stance-floating-mist.js
+++ source/modifiers/stance-floating-mist.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Floating Mist Stance",
 	"Increase Punch Stagger by @{stackCount*2} and gain @{stackCount*2} Evade each round.",
-	null,
+	"<:FloatingMistStance:1271518305521373327>",
 	true,
 	false,
 	0
--- source/modifiers/stance-iron-fist.js
+++ source/modifiers/stance-iron-fist.js
@@ -2,7 +2,7 @@ const { ModifierTemplate } = require("../classes");
 
 module.exports = new ModifierTemplate("Iron Fist Stance",
 	"Increase Punch damage by @{stackCount*45} and changes its element to yours.",
-	null,
+	"<:IronFistStance:1271518293445972038>",
 	true,
 	false,
 	0
