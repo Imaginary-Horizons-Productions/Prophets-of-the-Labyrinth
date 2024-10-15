015d05f9e22651f89b05af95c7e994e51c32ced1
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-10-14 22:29:14 -0700

    test command page generation

--- source/commands/commands.js
+++ source/commands/commands.js
@@ -2,8 +2,7 @@ const { InteractionContextType } = require('discord.js');
 const { CommandWrapper } = require('../classes');
 
 const mainId = "commands";
-module.exports = new CommandWrapper(mainId, "Get a link to a list of the Prophets of the Labyrinth commands", null, false, [InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel], 3000,
-	/** Link the user to the repo Commands wiki page (automatically updated) */
+module.exports = new CommandWrapper(mainId, "Get a link to the PotL commands wiki", null, false, [InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel], 3000,
 	(interaction) => {
 		interaction.reply({ content: "Here's a link to the Prophets of the Labyrinth commands page (automatically updated): https://github.com/Imaginary-Horizons-Productions/Prophets-of-the-Labyrinth/wiki/Commands", ephemeral: true });
 	}
