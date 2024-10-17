01b6a2edd70ef216f76c264f0d94805d15d4e8f3
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-10-17 11:27:29 -0700

    #277: add Loaded Dice - remove unused variable poolSize in readymove - fixed rn keys for random target selection - fixed dead enemy filter mutating indicies of cached enemy pool - early out of cacheRoundRn earlier

---
+++ source/artifacts/loadeddice.js
@@ -0,0 +1,7 @@
+const { ArtifactTemplate } = require("../classes");
+
+module.exports = new ArtifactTemplate("Loaded Dice",
+	"Moves the randomly select targets select @{copies} more.",
+	"Increase targets selected by 1 per dice set",
+	"Untyped"
+);
