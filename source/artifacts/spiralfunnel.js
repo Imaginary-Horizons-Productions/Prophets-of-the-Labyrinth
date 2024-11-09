76d8fc816d2b2fbede1eb107d30d7e8ed0d73973
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-11-08 16:04:04 -0800

    #434: clarify Spiral Funnel doesn't apply to Power Up (#454)

--- source/artifacts/spiralfunnel.js
+++ source/artifacts/spiralfunnel.js
@@ -1,7 +1,7 @@
 const { ArtifactTemplate } = require("../classes");
 
 module.exports = new ArtifactTemplate("Spiral Funnel",
-	"Increase modifier damage (eg Poison and Frail) dealt to enemies by @{copies*2} per stack.",
+	"Increase damage dealt to enemies by modifiers (eg Poison and Frail) by @{copies*2} per stack.",
 	"Increase damage per stack by 2 per funnel",
 	"Untyped"
 ).setFlavorText({ name: "*Artifact Usage Survey Report*", value: "*Found to be a major contributor to toxic spiralling in dungeon delves*" })
