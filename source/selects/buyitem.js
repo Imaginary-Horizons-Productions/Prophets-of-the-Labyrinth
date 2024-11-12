4749babb39059f2adad277938904a2f938a4b209
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-11-12 10:43:42 -0800

    add confirmation ephemerals to merchants

--- source/selects/buyitem.js
+++ source/selects/buyitem.js
@@ -1,7 +1,11 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 const { SelectWrapper } = require('../classes');
-const { SAFE_DELIMITER } = require('../constants');
+const { SAFE_DELIMITER, SKIP_INTERACTION_HANDLING } = require('../constants');
 const { getAdventure, setAdventure } = require('../orcustrators/adventureOrcustrator');
-const { renderRoom } = require('../util/embedUtil');
+const { renderRoom, randomAuthorTip } = require('../util/embedUtil');
+const { getColor } = require('../util/elementUtil');
+const { getItem } = require('../items/_itemDictionary');
+const { injectApplicationEmojiMarkdown } = require('../util/graphicsUtil');
 
 const mainId = "buyitem";
 module.exports = new SelectWrapper(mainId, 3000,
@@ -25,11 +29,36 @@ module.exports = new SelectWrapper(mainId, 3000,
 			return;
 		}
 
-		adventure.gold -= cost;
-		adventure.room.decrementResource(name, 1);
-		adventure.gainItem(name, 1);
-		interaction.message.edit(renderRoom(adventure, interaction.channel));
-		interaction.reply({ content: `${interaction.member.displayName} buys a ${name} for ${cost}g.` });
-		setAdventure(adventure);
+		const embed = new EmbedBuilder().setColor(getColor(adventure.room.element))
+			.setAuthor(randomAuthorTip())
+			.setTitle("Buy this item?")
+			.addFields({ name, value: injectApplicationEmojiMarkdown(getItem(name).description) });
+		interaction.reply({
+			embeds: [embed],
+			components: [
+				new ActionRowBuilder().addComponents(
+					new ButtonBuilder().setCustomId(SKIP_INTERACTION_HANDLING)
+						.setStyle(ButtonStyle.Success)
+						.setLabel(`Buy: ${cost}g`)
+				)
+			],
+			ephemeral: true,
+			fetchReply: true
+		}).then(reply => {
+			const collector = reply.createMessageComponentCollector({ max: 1 });
+			collector.on("collect", collectedInteraction => {
+				adventure.gold -= cost;
+				adventure.room.decrementResource(name, 1);
+				adventure.gainItem(name, 1);
+				interaction.message.edit(renderRoom(adventure, interaction.channel));
+				collectedInteraction.channel.send({ content: `${interaction.member.displayName} buys a ${name} for ${cost}g.` });
+				setAdventure(adventure);
+			})
+
+			collector.on("end", async (interactionCollection) => {
+				await interactionCollection.first().update({ components: [] });
+				interaction.deleteReply();
+			})
+		})
 	}
 );
