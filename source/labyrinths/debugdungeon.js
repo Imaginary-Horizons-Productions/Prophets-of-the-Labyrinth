19301a053b3155a5f6b747a5ffadbdea5e1a51de
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-12-31 11:19:10 -0800

    add gear to labyrinths

--- source/labyrinths/debugdungeon.js
+++ source/labyrinths/debugdungeon.js
@@ -16,29 +16,47 @@ module.exports = new LabyrinthTemplate("Debug Dungeon",
 		Darkness: Object.fromEntries(RARITIES.map(rarity => [rarity, [
 			"Fever Break",
 			"Fatiguing Fever Break",
-			"Unstoppable Fever Break"
+			"Unstoppable Fever Break",
+			"Warhammer",
+			"Fatiguing Warhammer",
+			"Vigorous Warhammer"
 	]])),
 		Earth: Object.fromEntries(RARITIES.map(rarity => [rarity, [
+			"Flail",
+			"Bouncing Flail",
+			"Incompatible Flail",
 			"Sandstorm Formation",
 			"Balanced Sandstorm Formation",
 			"Soothing Sandstorm Formation"
-	]])),
+		]])),
 		Fire: Object.fromEntries(RARITIES.map(rarity => [rarity, [
 			"Bonfire Formation",
 			"Charging Bonfire Formation",
-			"Hastening Bonfire Formation"
-	]])),
+			"Hastening Bonfire Formation",
+			"Longsword",
+			"Double Longsword",
+			"Reactive Longsword",
+		]])),
 		Light: Object.fromEntries(RARITIES.map(rarity => [rarity, [
+			"Battle Standard",
+			"Thief's Battle Standard",
+			"Tormenting Battle Standard",
 			"Reveal Flaw",
 			"Distracting Reveal Flaw",
 			"Numbing Reveal Flaw"
-	]])),
+		]])),
 		Water: Object.fromEntries(RARITIES.map(rarity => [rarity, [
 			"Conjured Ice Armaments",
 			"Supportive Conjured Ice Armaments",
-			"Vigilant Conjured Ice Armaments"
-	]])),
+			"Vigilant Conjured Ice Armaments",
+			"Net Launcher",
+			"Kinetic Net Launcher",
+			"Staggering Net Launcher",
+		]])),
 		Wind: Object.fromEntries(RARITIES.map(rarity => [rarity, [
+			"Greatsword",
+			"Chaining Greatsword",
+			"Distracting Greatsword",
 			"Tornado Formation",
 			"Charging Tornado Formation",
 			"Supportive Tornado Formation"
