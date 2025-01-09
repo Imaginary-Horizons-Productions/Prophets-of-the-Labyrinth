104ad630e07a856521d6315397dad259df53a9bb
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-01-09 14:18:22 -0800

    fix incorrect essence for Bejeweled Treasuresphere

--- source/artifacts/bejeweledtreasuresphere.js
+++ source/artifacts/bejeweledtreasuresphere.js
@@ -3,5 +3,5 @@ const { ArtifactTemplate } = require("../classes");
 module.exports = new ArtifactTemplate("Bejeweld Treasuresphere",
 	"Increase the adventure's score by @{copies*25}%.",
 	"Increase the score bonus percent by 25 per sphere",
-	"Untyped"
+	"Unaligned"
 );
