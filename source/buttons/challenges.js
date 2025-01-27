a56b13498c8023a2d4844ca1021ed4a2d6b48bd0
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-01-27 09:56:03 -0800

    #496: taking "Can't Hold All this Value" while at gear capacity forces a discard

--- source/buttons/challenges.js
+++ source/buttons/challenges.js
@@ -1,8 +1,8 @@
-const { ActionRowBuilder, StringSelectMenuBuilder, MessageFlags, DiscordjsErrorCodes } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder, MessageFlags, DiscordjsErrorCodes, ComponentType } = require('discord.js');
 const { ButtonWrapper, Challenge } = require('../classes');
 const { getAdventure, setAdventure } = require('../orcustrators/adventureOrcustrator');
 const { getChallenge } = require('../challenges/_challengeDictionary');
-const { trimForSelectOptionDescription } = require('../util/textUtil');
+const { trimForSelectOptionDescription, listifyEN } = require('../util/textUtil');
 const { SKIP_INTERACTION_HANDLING } = require('../constants');
 const { renderRoom } = require('../util/embedUtil');
 
@@ -34,16 +34,16 @@ module.exports = new ButtonWrapper(mainId, 3000,
 			components,
 			flags: [MessageFlags.Ephemeral],
 			withResponse: true
-		}).then(response => response.resource.message.awaitMessageComponent({ time: 120000 })).then(collectedInteraction => {
+		}).then(response => response.resource.message.awaitMessageComponent({ time: 120000, componentType: ComponentType.StringSelect })).then(collectedInteraction => {
 			const adventure = getAdventure(collectedInteraction.channelId);
 			const [_, startingDepth] = collectedInteraction.customId.split(SKIP_INTERACTION_HANDLING);
 			if (adventure?.room.history["New challenges"].length > 0 || startingDepth !== adventure.depth.toString()) {
 				return collectedInteraction;
 			}
 
-			const challengeName = collectedInteraction.values[0];
+			const [challengeName] = collectedInteraction.values;
 			const { intensity, duration, reward } = getChallenge(challengeName);
-			if (adventure.challenges[challengeName]) {
+			if (challengeName in adventure.challenges) {
 				adventure.challenges[challengeName].intensity += intensity;
 				adventure.challenges[challengeName].duration += duration;
 				adventure.challenges[challengeName].reward += reward;
@@ -52,7 +52,16 @@ module.exports = new ButtonWrapper(mainId, 3000,
 			}
 			adventure.room.history["New challenges"].push(challengeName);
 			interaction.message.edit(renderRoom(adventure, collectedInteraction.channel));
-			collectedInteraction.channel.send({ content: `The party takes on a new challenge: ${challengeName}` });
+			const dropMap = {};
+			if (challengeName === "Can't Hold All this Value") {
+				const reducedGearCapacity = adventure.getGearCapacity();
+				adventure.delvers.forEach(delver => {
+					if (delver.gear.length > reducedGearCapacity) {
+						dropMap[delver.name] = delver.gear.splice(reducedGearCapacity).map(gear => gear.name);
+					}
+				})
+			}
+			collectedInteraction.channel.send({ content: `The party takes on a new challenge: ${challengeName}${Object.entries(dropMap).map(([delverName, gearDropped]) => `\n- ${delverName} drops their ${listifyEN(gearDropped)}`).join("")}` });
 			setAdventure(adventure);
 			return collectedInteraction;
 		}).then(interactionToAcknowledge => {
