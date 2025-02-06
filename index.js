12d9883297bcfa19c80c95ecb4a76919d1489d8f
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-02-06 10:38:25 -0800

    add timestamp logging to sharding manager

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const { ShardingManager } = require("discord.js");
 
+const log = console.log;
+
+console.log = function () {
+	log.apply(console, [`<t:${Math.floor(Date.now() / 1000)}> `, ...arguments]);
+}
+
+const error = console.error;
+
+console.error = function () {
+	error.apply(console, [`<t:${Math.floor(Date.now() / 1000)}> `, ...arguments]);
+}
+
 const manager = new ShardingManager("./source/bot.js", {
 	token: require("./config/auth.json").token,
 	shardArgs: require("node:process").argv
