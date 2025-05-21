da767f88cf26f0019a98a33d89927e058945cd88
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-05-21 16:04:17 -0700

    #374: install `@sapphire/discord.js-utilities`

--- source/artifacts/enchantedmap.js
+++ source/artifacts/enchantedmap.js
@@ -1,9 +1,9 @@
+const { InteractionLimits } = require("@sapphire/discord.js-utilities");
 const { ArtifactTemplate } = require("../classes");
-const { MAX_BUTTONS_PER_ROW } = require("../constants");
 
 module.exports = new ArtifactTemplate("Enchanted Map",
 	"When picking rooms, roll @{copies} more option(s).",
-	`Increase number of rolls by 1 per map (duplicate rolls possible, ${MAX_BUTTONS_PER_ROW} unique options max)`,
+	`Increase number of rolls by 1 per map (duplicate rolls possible, ${InteractionLimits.MaximumButtonsPerActionRow} unique options max)`,
 	"Darkness",
 	300
 ).setFlavorText({ name: "*Additional Notes*", value: "*Makes routing real ez*" })
--- source/constants.js
+++ source/constants.js
@@ -1,5 +1,6 @@
 const authPath = "../config/auth.json";
 const { testGuildId, feedbackChannelId } = require(authPath);
+const { InteractionLimits } = require("@sapphire/discord.js-utilities");
 const { announcementsChannelId, lastPostedVersion } = require("../config/versionData.json");
 
 module.exports = {
@@ -10,19 +11,6 @@ module.exports = {
 	// Discord Constants
 	serverGuideMention: "<id:guide>",
 	channelBrowserMention: "<id:customize>",
-	MAX_MESSAGE_CONTENT_LENGTH: 2000,
-	MAX_EMBED_AUTHOR_NAME_LENGTH: 256,
-	MAX_EMBED_TITLE_LENGTH: 256,
-	MAX_EMBED_DESCRIPTION_LENGTH: 4096,
-	MAX_EMBED_FIELD_COUNT: 25,
-	MAX_EMBED_FIELD_NAME_LENGTH: 256,
-	MAX_EMBED_FIELD_VALUE_LENGTH: 1024,
-	MAX_EMBED_FOOTER_LENGTH: 2048,
-	MAX_EMBED_TOTAL_CHARACTERS: 6000,
-	MAX_EMBEDS_PER_MESSAGE: 10,
-	MAX_MESSAGE_ACTION_ROWS: 5,
-	MAX_BUTTONS_PER_ROW: 5,
-	MAX_SELECT_OPTIONS: 25,
 	DISCORD_ICON_URL: "https://cdn.discordapp.com/attachments/618523876187570187/1110265047516721333/discord-mark-blue.png",
 
 	// Discord Instance Constants
@@ -41,7 +29,7 @@ module.exports = {
 	GAME_VERSION: "v0.18.0",
 	POTL_ICON_URL: "https://images-ext-1.discordapp.net/external/wclKLsXO0RRUYVqULk4xBWnqyeepTl4MPdQAvwmYA4w/https/cdn.discordapp.com/avatars/950469509628702740/97f4ae84c14c2b88fbf569de061bac88.webp",
 	RN_TABLE_BASE: 16,
-	MAX_DELVER_COUNT: Math.floor(module.exports.MAX_SELECT_OPTIONS / 3),
+	MAX_DELVER_COUNT: Math.floor(InteractionLimits.MaximumOptionsInSelectMenus / 3),
 	EMPTY_SELECT_OPTION_SET: [{ label: "If the menu is stuck, switch channels and come back.", description: "This usually happens when two players use the same select at the same time.", value: "empty" }],
 	SURPASSING_VALUE: 250,
 	ESSENCE_MATCH_STAGGER_FOE: 2,
