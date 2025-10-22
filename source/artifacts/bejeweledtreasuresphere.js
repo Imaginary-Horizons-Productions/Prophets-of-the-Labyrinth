4df1b8e52a181431ff9458847380ea7b00659490
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-10-21 17:29:37 -0700

    fix typo in artifact name "Bejeweld Treasuresphere" -> "Bejeweled Treasuresphere"

--- source/artifacts/bejeweledtreasuresphere.js
+++ source/artifacts/bejeweledtreasuresphere.js
@@ -1,6 +1,6 @@
 const { ArtifactTemplate } = require("../classes");
 
-module.exports = new ArtifactTemplate("Bejeweld Treasuresphere",
+module.exports = new ArtifactTemplate("Bejeweled Treasuresphere",
 	"Increase the adventure's score by @{copies*25}%.",
 	"Increase the score bonus percent by 25 per sphere",
 	"Unaligned",
